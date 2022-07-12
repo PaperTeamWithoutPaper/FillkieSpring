7a4d682b285803b32bd5a3a463480c479109baa3
mwkangit <79822924+mwkangit@users.noreply.github.com>
2022-07-12 20:27:24 +0900

    chore: kill 80 port

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,10 @@
 REPOSITORY=/home/ubuntu/app
 cd $REPOSITORY
 
-APP_NAME=curriculum #1
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
-CURRENT_PID=$(pgrep -f $APP_NAME)
-
-if [ -z $CURRENT_PID ] #2
-then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
-else
-  echo "> kill -15 $CURRENT_PID"
-  sudo kill -15 $CURRENT_PID
-  sleep 5
-fi
+sudo lsof -t -i tcp:80 -s tcp:listen | sudo xargs kill
 
 echo "> $JAR_PATH 배포" #3
 sudo nohup java -jar $REPOSITORY/build/libs/fillkie-0.0.1-SNAPSHOT.jar --spring.config.location=/home/ubuntu/spring-app.yml > /home/ubuntu/log 2> /home/ubuntu/error &
