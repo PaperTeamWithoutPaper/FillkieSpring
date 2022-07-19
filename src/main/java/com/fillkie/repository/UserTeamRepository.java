ab4fdb7727206cbf20cef68aecf760b862e1477a
mwkangit <cabookis@gmail.com>
2022-07-19 15:16:20 +0900

    chore: teamService_isJoinUserTeam_optional_nullable

--- src/main/java/com/fillkie/repository/UserTeamRepository.java
+++ src/main/java/com/fillkie/repository/UserTeamRepository.java
@@ -1,6 +1,7 @@
 package com.fillkie.repository;
 
 import com.fillkie.domain.UserTeam;
+import com.mongodb.lang.Nullable;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -9,6 +10,7 @@ public interface UserTeamRepository extends MongoRepository<UserTeam, String> {
 
     public Optional<UserTeam> findById(String email);
     public List<UserTeam> findByUserId(String userId);
+    @Nullable
     public Optional<UserTeam> findByUserIdAndTeamId(String userId, String teamId);
 
 }
